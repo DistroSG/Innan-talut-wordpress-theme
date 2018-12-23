3552ba1b069f1108640f352fa6c1e9c6b9c13f6f
DistroSG <aleks.gradov@gmail.com>
2018-12-23 12:06:05 +0200

    Reorganized css structure. Moved header background image from html to css. Added sidebar area to template.

--- functions.php
+++ functions.php
@@ -1,15 +1,40 @@
 <?php
+
+add_action('wp_enqueue_scripts', 'add_theme_scripts');
+
 function add_theme_scripts() {
-  wp_enqueue_style( 'style', get_stylesheet_uri() );
+  wp_register_style('theme-styles', get_template_directory_uri() . '/css/style.css');
+  wp_enqueue_style('theme-styles');
 }
 
-add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
-
-add_theme_support( 'custom-header', array(
-  'default-image'      => '',
-  'width'              => 1920,
-  'height'             => 300,
-  'flex-height'        => true,
+add_theme_support('custom-header', array(
+  'default-image' => '',
+  'width' => 1920,
+  'height' => 300,
+  'flex-height' => true,
 ));
+
+add_action('wp_loaded', 'generate_header');
+
+function generate_header() {
+
+  if ( !is_customize_preview() ) {
+    return;
+  }
+
+  $file_path = get_template_directory() . '/scss/_generatedHeader.scss';
+  $file_content = file_get_contents($file_path);
+
+  $content = "#masthead{";
+  $content .= "background-image: url(" . get_header_image() . ");";
+  $content .= "height:" . get_custom_header()->height . "px;";
+  $content .= "}";
+
+  if( preg_replace('/\s+/','',$content) === preg_replace('/\s+/','',$file_content) ){
+    return;
+  }
+
+  file_put_contents($file_path, $content);
+}
 
 ?>
